b4792e00c997492847cd7c30b04844ec729082a1
barolad <97976691+barolad@users.noreply.github.com>
2022-02-10 00:46:21 +0300

    Delete sq_db.sql

--- sq_db.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-id integer PRIMARY KEY AUTOINCREMENT,
-username text NOT NULL,
-firstname text NOT NULL,
-email text NOT NULL,
-psw text NOT NULL,
-avatar BLOB DEFAULT NULL,
-time integer NOT NULL
-);
